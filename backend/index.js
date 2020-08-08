252e5352eebd00bb0af766b01fdf525403c32aee
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-09 02:49:16 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+
+//Make database connectio
+mongoose.connect('mongodb+srv://mickey:mick7797@cluster0.ypmxn.mongodb.net/RestAPI?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connect to DB');
+    });
 
 const Port = process.env.port || 5000;
 
