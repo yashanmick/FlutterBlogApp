8cfb9b0a5ce6b862bfcc618417d7371c6f0300f5
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-09 10:05:45 +0530

    routes updated

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Port = process.env.port || 5000;
 
 //Make database connectio
-mongoose.connect('mongodb+srv://mickey:mick7797@cluster0.ypmxn.mongodb.net/RestAPI?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true },
+mongoose.connect('mongodb+srv://mickey:mick7797@cluster0.ypmxn.mongodb.net/TestRestAPI?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true },
     () => {
         console.log('Connect to DB');
-    });
+    }
+);
 
-const Port = process.env.port || 5000;
+// mongoose.connect('mongodb+srv://mickey:mick7797@cluster0.ypmxn.mongodb.net/TestRestAPI?retryWrites=true&w=majority',
+//     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }
+// );
+
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('Connect to DB');
+// });
+
+
+//middleware
+app.use(express.json());
+const userRoute = require("./routes/user");
+app.use("/user", userRoute);
 
 app.route('/').get((req, res) => res.json('your first rest api'));
 
